23dd7c0bced813ad66b0d8792c132a0afefb7c0d
BarZamsky <barzamsky1@gmail.com>
2017-11-22 19:06:49 +0200

    change function parse_file- create Time element

--- src/matala_1/ParsingFiles.java
+++ src/matala_1/ParsingFiles.java
@@ -19,7 +19,8 @@ public class ParsingFiles extends ReadFiles{
 		int countNet =0;
 		for (int i = 2; i < file.size(); i+=countNet) {
 			String[]s = file.get(i);
-			Time time = new Time(s[3]);
+			Time time =new Time();
+			time = time.set_Date(s[3]);
 			List<Network> Wifi = new ArrayList<Network>();
 			double lat = Double.parseDouble(s[6]);
 			double lon = Double.parseDouble(s[7]);
@@ -29,8 +30,9 @@ public class ParsingFiles extends ReadFiles{
 			countNet =0;
 			for (int j = 2; j < file.size(); j++) {
 				String[] lines = file.get(j);
-				Time t = new Time(lines[3]);
-				if (t.equal_Time(time)){
+				Time time2 = new Time();
+				time2=time2.set_Date(lines[3]);
+				if (time2.is_Equal(time)){
 					Network wifi = new Network(lines[1],lines[0],lines[5],lines[4]);
 					Wifi.add(wifi);
 					countNet++;	
@@ -58,16 +60,16 @@ public class ParsingFiles extends ReadFiles{
 	}
 
 	public static void main(String[] args) {
-		Folder f  = new Folder("C:\\Users\\a\\git\\OO_Project\\ObjectOriented") ;
-		String []s = f.csv_Files("C:\\Users\\a\\git\\OO_Project\\ObjectOriented");
+	//	Folder f  = new Folder("C:\\Users\\a\\git\\OO_Project\\ObjectOriented") ;
+		String []s = Folder.csv_Files("C:\\Users\\a\\git\\OO_Project\\ObjectOriented");
 		System.out.println(s);
 		List<LineFile> fin = new ArrayList<LineFile>();
 		fin = merge_File(s);
 		WriteCsv.writeFile(fin, "fin.csv");
-		String m="SM-G950F";
-		filter c2 = list->list.getModel().equals(m);
+		//filter c3 = list->list.getTime().getMinute()==22;
+		//filter c2 = list->list.getModel().toString().equals("SM-G950F");
 		filter c1 = list->list.getLocation().getPoints().getAlt()>=650;
-		List<LineFile> filteredStrings = FileFilter.filter(fin,c2);
+		List<LineFile> filteredStrings = FileFilter.filter(fin,c1);
 		WriteCsv.writeFile(filteredStrings,"filterList.csv");
 	}
 }
