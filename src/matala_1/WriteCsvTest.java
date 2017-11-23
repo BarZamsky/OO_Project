dbdee4d8e48d5334e061c30ad20ba232096ca756
noytvili <noytvili93@gmail.com>
2017-11-23 13:51:04 +0200

    i don't know how to test this function, i tried some but with no success. we need to check that.

---
+++ src/matala_1/WriteCsvTest.java
@@ -0,0 +1,33 @@
+package matala_1;
+
+import static org.junit.Assert.*;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class WriteCsvTest {
+	
+	@Test
+	public void test() {
+	/*
+		String[] files = Folder.csv_Files("C:/Users/Doriya Spielman/git/OO_Project");
+		String []s = files.csv_Files("C:\\Users\\a\\git\\OO_Project\\ObjectOriented");
+		List<LineFile> fin = new ArrayList<LineFile>();
+		fin = merge_File(s);
+		WriteCsv.writeFile(fin, "fin.csv");
+		WriteCsv.writeFile(fin, "fin.csv");
+		
+		assertEquals("New file size that written is: ", 149, fin.size());
+		
+	
+		//check for not null value
+		assertNotNull(fin);
+		
+		*/
+	}
+
+	
+}
