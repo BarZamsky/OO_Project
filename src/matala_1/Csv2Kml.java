c1a76740327f9aba486c9c3a28362f03f5bd8e90
BarZamsky <barzamsky1@gmail.com>
2017-11-23 20:25:02 +0200

    changed kml function. addad TimeStamp

--- src/matala_1/Csv2Kml.java
+++ src/matala_1/Csv2Kml.java
@@ -1,8 +1,11 @@
 package matala_1;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Folder;
@@ -11,6 +14,7 @@ import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.Placemark;
 import de.micromata.opengis.kml.v_2_2_0.Style;
 import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
+import de.micromata.opengis.kml.v_2_2_0.Feature;
 
 /**
  * This class converts csv file to a KML file with a placemark and a chart for each continent. The chart is generated with the Google Chart API and
@@ -20,64 +24,48 @@ import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
  *
  */
 public class Csv2Kml {
-	/**
-	 * The createPlacemarkWithChart ()-method generates and set a placemark object, with the given statistical data . The Icon and Style
-	 * objects (color and size of the text and icon) are saved to the root element. The placemark is created and set to the given folder.
-	 * @param document structure of the KML file
-	 * @param folder to add continent
-	 * @param time of the continent
-	 * @param ID of the mobile phone
-	 * @param ssid of the continent
-	 * @param MAC of the continent
-	 * @param Signal of the continent
-	 * @param Frequency of the continent
-	 * @param lon of the continent
-	 * @param lat of the continent
-	 */
-	private static void createPlacemarkWithChart(Document document, Folder folder,String time, String ID,String ssid,String MAC, 
-			String Signal,String Frequency, double lon, double lat ) {
-		
-		Style style = document.createAndAddStyle();
-		style.createAndSetLabelStyle().withColor("7fffaaff").withScale(1.5);
-		style.withId("style_" + ID) // set the stylename to use this style from the placemark
-		.createAndSetIconStyle().withScale(1.0).withIcon(new Icon().withHref("http://maps.google.com/mapfiles/kml/paddle/ltblu-stars.png")); // set size and icon
-		style.createAndSetLabelStyle().withColor("ff43b3ff").withScale(1.0); // set color and size of the continent name
-
-		Placemark placemark = folder.createAndAddPlacemark();
-		TimeStamp stamp = placemark.createAndSetTimeStamp();
-		stamp.setWhen(time);
-		// use the style for each continent
-		placemark.withName(ID)
-		.withStyleUrl("#style_" + ID)
-		// 3D chart image
-		.withDescription(
-				"Time: <b>"+ time +"</b><br/>ID: <b>"+ ID +"</b><br/>SSID: <b>"+ ssid +"</b><br/>MAC: <b>"+ MAC +"</b><br/>Signal: <b>"+ Signal +"</b><br/>Frequency: <b>"+ Frequency +"</b>")
-		// coordinates and distance (zoom level) of the viewer
-		.createAndSetLookAt().withLongitude(lon).withLatitude(lat).withAltitude(0);
-
-		placemark.createAndSetPoint().addToCoordinates(lon, lat); // set coordinates
-	}
-
-	public static void main(String[] args) throws IOException {
+/**
+ * This fumction creates a KML file
+ * @param lines the LineFile List
+ * @param output the name of the new kml file
+ */
+	private static void create_Kml(List<LineFile> lines, String output) {
 		final Kml kml = new Kml();
-		String name = "C:/Users/a/git/OO_Project/ObjectOriented/filterList.csv";
-		Document doc = kml.createAndSetDocument().withName("JAK Example1").withOpen(true);
+		Document doc = kml.createAndSetDocument().withName("Locations").withOpen(true);
 		Folder folder = doc.createAndAddFolder();
 		folder.withName("Wifi Networks").withOpen(true);
-		
-		try(BufferedReader br = new BufferedReader(new FileReader(name))){	
-			br.readLine();
-
-			while(br.readLine() != null){
-				String [] s =br.readLine().split(",");
-				double lat = Double.parseDouble(s[2]);
-				double lon = Double.parseDouble(s[3]);
-				for (int i = 6; i < s.length; i+=4) {
-						createPlacemarkWithChart(doc, folder, s[0] ,s[1], s[i++], s[i++],s[i++], s[i++],  lon	,lat );
-				}	
+		Style style = doc.createAndAddStyle();
+		style.createAndSetLabelStyle().withColor("7fffaaff").withScale(1.5);
+		style.withId("Our_OOP")
+		.createAndSetIconStyle().withScale(1.0).withIcon(new Icon().withHref("http://maps.google.com/mapfiles/kml/paddle/ltblu-stars.png"));
+		style.createAndSetLabelStyle().withColor("ff43b3ff").withScale(1.0);
+		for(LineFile f : lines){
+			doc.createAndSetTimeStamp().withWhen(f.getTime().toString());
+			Placemark placemark=folder.createAndAddPlacemark();
+			List<Network> net=f.getNetwork();
+			for(Network n : net){
+				placemark.createAndSetTimeStamp().withWhen(f.getTime().toString());
+				placemark.withName(f.getModel().toString())
+				//.withStyleUrl("#style_" + ID)
+				.withDescription(
+						"Time: <b>"+ f.getTime().toString() +"</b><br/>ID: <b>"+ f.getModel().toString() +"</b><br/>SSID: <b>"+n.getSSID().toString() +"</b><br/>MAC: <b>"+ n.getMac().toString() +"</b><br/>Signal: <b>"+ n.getSignal().toString() +"</b><br/>Frequency: <b>"+ n.getChanel().toString() +"</b>")
+				.createAndSetLookAt().withLongitude(f.getLocation().getPoints().getLon()).withLatitude(f.getLocation().getPoints().getLat()).withAltitude(f.getLocation().getPoints().getAlt());
+				doc.addToFeature(placemark);
 			}
+			placemark.createAndSetPoint().addToCoordinates(f.getLocation().getPoints().getLon(),f.getLocation().getPoints().getLat(),f.getLocation().getPoints().getAlt());
 		}
-		kml.marshal(new File("C:\\Users\\a\\workspace\\ObjectOriented\\final2.kml"));
+		kml.setFeature(doc);
+		try {
+			kml.marshal(new File(output));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		String []s = matala_1.Folder.csv_Files("C:\\Users\\a\\git\\OO_Project\\ObjectOriented");
+		List<LineFile> fin = new ArrayList<LineFile>();
+		fin = matala_1.ParsingFiles.merge_File(s);
+		create_Kml(fin, "try1.kml");
 	}
 }
 
