715c17baafbd02f56b278d2c2f7bccddbf76be41
noytvili <noytvili93@gmail.com>
2017-12-11 22:15:11 +0200

    test for Algo1_line

--- src/matala_1/Algo1_line.java
+++ src/matala_1/Algo1_line.java
@@ -6,45 +6,44 @@ package matala_1;
  */
 public class Algo1_line implements Comparable<Algo1_line> {
 
-private int signal;
-private double alt;
-Point_2D location;
-public Algo1_line(int signal, double alt, Point_2D location) {
-	this.signal = signal;
-	this.alt = alt;
-	this.location = location;
-}
+	private int signal;
+	private double alt;
+	Point_2D location;
+	
+	public Algo1_line(int signal, double alt, Point_2D location) {
+		this.signal = signal;
+		this.alt = alt;
+		this.location = location;
+	}
 
-public int getSignal() {
-	return signal;
-}
-public void setSignal(int signal) {
-	this.signal = signal;
-}
-public double getAlt() {
-	return alt;
-}
-public void setAlt(double alt) {
-	this.alt = alt;
-}
+	public int getSignal() {
+		return signal;
+	}
+	public void setSignal(int signal) {
+		this.signal = signal;
+	}
+	public double getAlt() {
+		return alt;
+	}
+	public void setAlt(double alt) {
+		this.alt = alt;
+	}
 
-public Point_2D getLocation() {
-	return location;
-}
+	public Point_2D getLocation() {
+		return location;
+	}
 
-public void setLocation(Point_2D location) {
-	this.location = location;
-}
+	public void setLocation(Point_2D location) {
+		this.location = location;
+	}
 
-@Override
-public int compareTo(Algo1_line o) {
-	return Integer.compare(-this.signal, -o.signal);
-}
+	@Override
+	public int compareTo(Algo1_line o) {
+		return Integer.compare(-this.signal, -o.signal);
+	}
 
-@Override
-public String toString() {
-	return "Algo1_line [signal=" + signal + ", alt=" + alt + ", location=" + location + "]";
-}
-
-
+	@Override
+	public String toString() {
+		return "Algo1_line [signal=" + signal + ", alt=" + alt + ", location=" + location + "]";
+	}
 }
