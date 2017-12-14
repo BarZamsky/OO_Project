fc4947878201e9bf79381e4e961701c94e897b28
BarZamsky <barzamsky1@gmail.com>
2017-12-14 21:20:27 +0200

    add compare excel file for algo1

--- src/Project/Algo_1.java
+++ src/Project/Algo_1.java
@@ -2,14 +2,13 @@ package Project;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 /**
- * This class represents functions to calculate w-center by MAC address
+ * This class represents functions to read merge CSV file and calculate w-center for every mac address in it
  * @author Doriya , Noy, Bar
  *
  */
@@ -27,14 +26,13 @@ public class Algo_1{
 		_file = new ArrayList<LineFile>();
 	}
 	/**
-	 * This functions search for the MAC in the Records list, takes 4 MAC with strongest signal 
-	 * and calculate the wLat,wLon,wAlt according to the demands
+	 * This function go over the list and for every mac address in it 
+	 * calculate the wLat,wLon,wAlt according to the demands
 	 */
 	public void locate_Mac(){
 		_fileList = new ArrayList<Algo1_linefile>();
 		for(LineFile l : _file){
 			List<Algo1_line> line = new ArrayList<Algo1_line>();
-			
 			_wLat = new ArrayList<Double>();
 			_wLon = new ArrayList<Double>();
 			_wAlt = new ArrayList<Double>();
@@ -43,37 +41,34 @@ public class Algo_1{
 			for(Network n : wifi){
 				List<Network> net = new ArrayList<Network>();
 				if(n.isTaken()==false){
-				String mac = n.getMac();
+					String mac = n.getMac();
 					net.add(n);
 					line = search(mac, _file);
-				
-			line.sort(null);
-			for(int i=0;i<max_Signals && i<line.size();i++){
-				double lat = line.get(i).getLocation().getLat();
-				double lon = line.get(i).getLocation().getLon();
-				double alt = line.get(i).getAlt();
-				double weight = 1/(Math.pow(line.get(i).getSignal(), 2));
-				_wLat.add(lat*weight);
-				_wLon.add(lon*weight);
-				_wAlt.add(alt*weight);
-				_wWeigth.add(weight);
+
+					line.sort(null);
+					for(int i=0;i<max_Signals && i<line.size();i++){
+						double lat = line.get(i).getLocation().getLat();
+						double lon = line.get(i).getLocation().getLon();
+						double alt = line.get(i).getAlt();
+						double weight = 1/(Math.pow(line.get(i).getSignal(), 2));
+						_wLat.add(lat*weight);
+						_wLon.add(lon*weight);
+						_wAlt.add(alt*weight);
+						_wWeigth.add(weight);
+					}
+
+					calc_Wsum();
+					_fileList.add(new Algo1_linefile(l.getTime(), new Point_2D(wLon, wLat), wAlt, net.get(0)));
+				}
 			}
-			
-			calc_Wsum();
-			_fileList.add(new Algo1_linefile(l.getTime(), new Point_2D(wLon, wLat), wAlt, net.get(0)));
-			}
-			}
-			}
-		for(Algo1_linefile a: _fileList){
-			System.out.println(a.toString());
 		}
 	}
-/**
- * This function search in the list for same mac address as given 
- * @param mac the mac to search in the list
- * @param _file LineFile list
- * @return new Algo1_line list
- */
+	/**
+	 * This function search in the list for same mac address as given 
+	 * @param mac the mac to search in the list
+	 * @param _file LineFile list
+	 * @return new Algo1_line list
+	 */
 	public List<Algo1_line> search(String mac, List<LineFile> _file){
 		List<Algo1_line> line = new ArrayList<Algo1_line>();;
 		for(LineFile _list : _file){
@@ -125,7 +120,7 @@ public class Algo_1{
 	 * @param fileName merged file name
 	 */
 	public void readFile(String fileName){
-		List<LineFile> files = new ArrayList<LineFile>();
+		_file = new ArrayList<LineFile>();
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			String line="";
@@ -140,7 +135,7 @@ public class Algo_1{
 					Network n = new Network(str[i], str[i+1], Integer.parseInt(str[i+2]), str[i+3]);
 					net.add(n);
 				}
-				files.add(new LineFile(time,str[1],point,Double.parseDouble(str[4]),Integer.parseInt(str[5]), net));
+				_file.add(new LineFile(time,str[1],point,Double.parseDouble(str[4]),Integer.parseInt(str[5]), net));
 			}
 			br.close();
 		}
@@ -151,7 +146,7 @@ public class Algo_1{
 	}
 	public static void main(String[] args) {
 		Algo_1 a = new Algo_1();
-		a.readFile("Merge_File.csv");
+		a.readFile("comb_BM3.csv");
 		a.locate_Mac();
 		a.toCsv2("algo1.csv");
 	}
