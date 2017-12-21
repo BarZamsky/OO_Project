44fc9d2d42f09de9f6bfc12f132a2851540b2ee0
noytvili <noytvili93@gmail.com>
2017-12-21 21:55:07 +0200

    Parameters- final

--- src/Project/Parameters.java
+++ src/Project/Parameters.java
@@ -1,5 +1,9 @@
 package Project;
 
+/**
+ * this class represents all the constants Parameters
+ * @author Noy, Doriya, Bar
+ */
 public class Parameters {
 	static final int NO_SIGNAL = -120;
 	static final int POWER = 2;
@@ -8,4 +12,25 @@ public class Parameters {
 	static final int MIN_DIFF = 3;
 	static final int DIFF_NO_SIG = 100;
 	static final int max_Signals = 4;
+	public static int getNoSignal() {
+		return NO_SIGNAL;
+	}
+	public static int getPower() {
+		return POWER;
+	}
+	public static int getNorm() {
+		return NORM;
+	}
+	public static double getSigDiff() {
+		return SIG_DIFF;
+	}
+	public static int getMinDiff() {
+		return MIN_DIFF;
+	}
+	public static int getDiffNoSig() {
+		return DIFF_NO_SIG;
+	}
+	public static int getMaxSignals() {
+		return max_Signals;
+	}
 }
