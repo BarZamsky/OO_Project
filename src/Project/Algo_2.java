f4d8678c8e68ddf9a8de1073cb7ce395b88d1f17
noytvili <noytvili93@gmail.com>
2017-12-21 23:30:21 +0200

    update the EX2 explanation

--- src/Project/Algo_2.java
+++ src/Project/Algo_2.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -30,7 +29,6 @@ public class Algo_2 implements Functions{
 	 * return the new location in the Input file
 	 */
 	public void search_Mac(){
-		boolean exist=false;
 		for(LineFile line_input : _input){
 			_list = new ArrayList<List<Algo2_line>>();
 			_comb = new ArrayList<Algo2_calc>();
@@ -46,170 +44,148 @@ public class Algo_2 implements Functions{
 							l.add(al);
 						}
 						else{
-//							for(LineFile line_data_Null : _data){ //for to check null
-//								List<Network> _wifi_Null = line_data_Null.getNetwork();
-//
-//								for(Network wifi_null : _wifi_Null){
-//
-//									if(wifi_null.getMac().equals(mac_input)){
-//										exist=true;
-//									}
-//									else exist=false;
-//								}
-//							}
-//						
-//
-//						if(exist) {
 							Algo2_line al =new Algo2_line(line_data,net.getSignal(),NO_SIGNAL);
 							l.add(al);
 						}
-//						else {
-//							Algo2_line al =new Algo2_line(0);
-//							l.add(al);
-//						}
-//					}
+					}
+				}
+				_list.add(l);
+			}
+			int wifi_Number = _list.size();// the List<List> size
+			int list_size = _list.get(0).size();//each list size
+
+			for(int j=0;j<list_size;j++){
+				double pi = 1.0;
+				for(int i =0;i<wifi_Number;i++){
+					List<Algo2_line> ls = _list.get(i);
+					Algo2_line alg = ls.get(j);
+					pi*=alg.get_weight();
+					_point = alg.get_p();
+					_alt = alg.getAlt();
+				}
+				_comb.add(new Algo2_calc(_point,_alt,pi));
+			}
+			_comb.sort(null);
+			calc_Weight();
+			line_input.setAlt(w_alt);
+			line_input.setLocation(new Point_2D(w_lon, w_lat));
+		}
+	}
+
+	/**
+	 * this function calculate the wLat,wLon,wAlt according to the demands
+	 * and the final w_alt, w_lat, w_lon to create a new location
+	 */
+	public void calc_Weight(){
+		double sum_wAlt = 0, sum_wLon = 0, sum_wLat = 0, sum_Weight = 0;
+		for (int i=0; i < 3 && i<_comb.size(); i++){
+
+			sum_wAlt += _comb.get(i).get_alt()*_comb.get(i).get_pi();
+			sum_wLon += _comb.get(i).get_point().getLon()*_comb.get(i).get_pi();
+			sum_wLat += _comb.get(i).get_point().getLat()*_comb.get(i).get_pi();
+			sum_Weight+= _comb.get(i).get_pi();
+		}
+		w_alt=sum_wAlt/sum_Weight;
+		w_lon=sum_wLon/sum_Weight;
+		w_lat=sum_wLat/sum_Weight;
+	}
+
+	/**
+	 * Reading from no GPS file.
+	 * @param fileName
+	 * @return List
+	 */
+	@Override
+	public void readFile(String fileName){
+		_input = new ArrayList<LineFile>();
+		try{
+			FileReader fr = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(fr);
+			String line="";
+			while ((line = br.readLine()) != null) {
+				String[] str = line.split(",");
+				List<Network> net = new ArrayList<Network>();
+				Time time = new Time();
+				time = time.set_Date(str[0]);
+				for(int i=6;i<str.length;i+=4){
+					Network n = new Network(str[i], str[i+1], Integer.parseInt(str[i+3]), str[i+2]);
+					net.add(n);
+				}
+				net.sort(null);
+				if(str[2].equals("?") ||str[3].equals("?") ||str[4].equals("?")){
+					Point_2D point = new Point_2D(0,0);
+					double alt = 0;
+					_input.add(new LineFile(time,str[1],point,alt,Integer.parseInt(str[5]), net));
+				}
+				else{
+					Point_2D point = new Point_2D(Double.parseDouble(str[2]),Double.parseDouble(str[3]));
+					double alt =Double.parseDouble(str[4]);
+					_input.add(new LineFile(time,str[1],point,alt,Integer.parseInt(str[5]), net));
 				}
 			}
-			_list.add(l);
+			fr.close();
+			br.close();
 		}
-		int wifi_Number = _list.size();// the List<List> size
-		int list_size = _list.get(0).size();//each list size
-
-		for(int j=0;j<list_size;j++){
-			double pi = 1.0;
-			for(int i =0;i<wifi_Number;i++){
-				List<Algo2_line> ls = _list.get(i);
-				Algo2_line alg = ls.get(j);
-				pi*=alg.get_weight();
-				_point = alg.get_p();
-				_alt = alg.getAlt();
+		catch(IOException ex) {
+			System.out.print("Errorg reading file\n" + ex);
+			System.exit(2);
+		}
+	}
+	/**
+	 * Reading from merge input file (my merge file not boaz)
+	 * @param fileName
+	 */
+	public void readFile2(String fileName){
+		_data = new ArrayList<LineFile>();
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			String line=""; 
+			br.readLine();   
+			while ((line = br.readLine()) != null) {
+				String[] str = line.split(",");
+				List<Network> net = new ArrayList<Network>();
+				Point_2D point = new Point_2D(Double.parseDouble(str[3]),Double.parseDouble(str[2]));
+				Time time = new Time();
+				time = time.set_Date(str[0]);
+				for(int i=6;i<str.length;i+=4){ 
+					Network n = new Network(str[i], str[i+1], Integer.parseInt(str[i+2]), str[i+3]);
+					net.add(n);
+				} 
+				_data.add(new LineFile(time,str[1],point,Double.parseDouble(str[4]),Integer.parseInt(str[5]), net));
 			}
-			_comb.add(new Algo2_calc(_point,_alt,pi));
+			br.close();
 		}
-		_comb.sort(null);
-		calc_Weight();
-		line_input.setAlt(w_alt);
-		line_input.setLocation(new Point_2D(w_lon, w_lat));
+		catch(IOException ex) {
+			System.out.print("Error Algo2 reading comb file\n" + ex);
+			System.exit(2);
+		}
 	}
-}
 
-/**
- * this function calculate the wLat,wLon,wAlt according to the demands
- * and the final w_alt, w_lat, w_lon to create a new location
- */
-public void calc_Weight(){
-	double sum_wAlt = 0, sum_wLon = 0, sum_wLat = 0, sum_Weight = 0;
-	for (int i=0; i < 3 && i<_comb.size(); i++){
-		
-		sum_wAlt += _comb.get(i).get_alt()*_comb.get(i).get_pi();
-		sum_wLon += _comb.get(i).get_point().getLon()*_comb.get(i).get_pi();
-		sum_wLat += _comb.get(i).get_point().getLat()*_comb.get(i).get_pi();
-		sum_Weight+= _comb.get(i).get_pi();
-	
-	}
-	w_alt=sum_wAlt/sum_Weight;
-	w_lon=sum_wLon/sum_Weight;
-	w_lat=sum_wLat/sum_Weight;
-
-
-}
-
-/**
- * Reading from no GPS file.
- * @param fileName
- * @return List
- */
-@Override
-public void readFile(String fileName){
-	_input = new ArrayList<LineFile>();
-	try{
-		FileReader fr = new FileReader(fileName);
-		BufferedReader br = new BufferedReader(fr);
-		String line="";
-		while ((line = br.readLine()) != null) {
-			String[] str = line.split(",");
-			List<Network> net = new ArrayList<Network>();
-			Time time = new Time();
-			time = time.set_Date(str[0]);
-			for(int i=6;i<str.length;i+=4){
-				Network n = new Network(str[i], str[i+1], Integer.parseInt(str[i+3]), str[i+2]);
-				net.add(n);
+	/**
+	 * This function writes the new CSV file
+	 * @param output output CSV file name
+	 */
+	@Override
+	public void toCsv(String output){
+		try{
+			FileWriter fw = new FileWriter(output);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for(LineFile l : _input){
+				bw.write(l.toString().replace("[", "").replace("]", ""));
+				bw.write("\n");
 			}
-			net.sort(null);
-			if(str[2].equals("?") ||str[3].equals("?") ||str[4].equals("?")){
-				Point_2D point = new Point_2D(0,0);
-				double alt = 0;
-				_input.add(new LineFile(time,str[1],point,alt,Integer.parseInt(str[5]), net));
-			}
-			else{
-				Point_2D point = new Point_2D(Double.parseDouble(str[2]),Double.parseDouble(str[3]));
-				double alt =Double.parseDouble(str[4]);
-				_input.add(new LineFile(time,str[1],point,alt,Integer.parseInt(str[5]), net));
-			}
+			bw.close();
 		}
-		fr.close();
-		br.close();
-	}
-	catch(IOException ex) {
-		System.out.print("Errorg reading file\n" + ex);
-		System.exit(2);
-	}
-}
-/**
- * Reading from merge input file (my merge file not boaz)
- * @param fileName
- */
-public void readFile2(String fileName){
-	_data = new ArrayList<LineFile>();
-	try{
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		String line=""; 
-		br.readLine();   
-		while ((line = br.readLine()) != null) {
-			String[] str = line.split(",");
-			List<Network> net = new ArrayList<Network>();
-			Point_2D point = new Point_2D(Double.parseDouble(str[3]),Double.parseDouble(str[2]));
-			Time time = new Time();
-			time = time.set_Date(str[0]);
-			for(int i=6;i<str.length;i+=4){ 
-				Network n = new Network(str[i], str[i+1], Integer.parseInt(str[i+2]), str[i+3]);
-				net.add(n);
-			} 
-			_data.add(new LineFile(time,str[1],point,Double.parseDouble(str[4]),Integer.parseInt(str[5]), net));
+		catch(IOException ex) {
+			System.out.print("Error writing file\n" + ex);
 		}
-		br.close();
 	}
-	catch(IOException ex) {
-		System.out.print("Error Algo2 reading comb file\n" + ex);
-		System.exit(2);
-	}
-}
 
-/**
- * This function writes the new csv file
- * @param output output csv file name
- */
-@Override
-public void toCsv(String output){
-	try{
-		FileWriter fw = new FileWriter(output);
-		BufferedWriter bw = new BufferedWriter(fw);
-		for(LineFile l : _input){
-			bw.write(l.toString().replace("[", "").replace("]", ""));
-			bw.write("\n");
-		}
-		bw.close();
+	public static void main(String[] args) {
+		Algo_2 a = new Algo_2();
+		a.readFile("_comb_no_gps_ts2_.csv");
+		a.readFile2("_comb_all_BM3_.csv");
+		a.search_Mac();
+		a.toCsv("complete_File_Algo_2.csv");
 	}
-	catch(IOException ex) {
-		System.out.print("Error writing file\n" + ex);
-	}
-}
-
-public static void main(String[] args) {
-	Algo_2 a = new Algo_2();
-	a.readFile("_comb_no_gps_ts2_.csv");
-	a.readFile2("_comb_all_BM3_.csv");
-	a.search_Mac();
-	a.toCsv("complete_File_Algo_2.csv");
-}
 }
