7e37918043b73eca6c4f800bd5ae2a1ee25ede12
noytvili <noytvili93@gmail.com>
2017-12-25 00:22:16 +0200

    icons for GUI

--- src/GUI/ourGUI.java
+++ src/GUI/ourGUI.java
@@ -1,23 +1,14 @@
 package GUI;
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JButton;
-import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextPane;
-import java.awt.Color;
-import javax.swing.JTextField;
-import javax.swing.JList;
 import javax.swing.JLabel;
 import java.awt.Font;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.ImageIcon;
 
 public class ourGUI {
 
@@ -57,68 +48,77 @@ public class ourGUI {
 		frmOop.getContentPane().setLayout(null);
 		
 		JButton btnNameOfScv = new JButton("Name of CSV file");
+		btnNameOfScv.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/file.png")));
 		btnNameOfScv.setFont(new Font("Gisha", Font.PLAIN, 22));
 		btnNameOfScv.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNameOfScv.setBounds(42, 138, 217, 37);
+		btnNameOfScv.setBounds(42, 138, 253, 37);
 		frmOop.getContentPane().add(btnNameOfScv);
 		
-		JButton btnNameOfFolder = new JButton("Name of folder");
+		JButton btnNameOfFolder = new JButton("  Name of folder");
+		btnNameOfFolder.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/folder.png")));
 		btnNameOfFolder.setFont(new Font("Gisha", Font.PLAIN, 22));
-		btnNameOfFolder.setBounds(42, 77, 217, 37);
+		btnNameOfFolder.setBounds(42, 77, 253, 37);
 		frmOop.getContentPane().add(btnNameOfFolder);
 		
-		JButton btnInformation = new JButton("Information");
+		JButton btnInformation = new JButton("  Information");
+		btnInformation.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/info.png")));
 		btnInformation.setFont(new Font("Gisha", Font.PLAIN, 22));
 		btnInformation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnInformation.setBounds(42, 260, 217, 37);
+		btnInformation.setBounds(42, 260, 253, 37);
 		frmOop.getContentPane().add(btnInformation);
 		
-		JButton btnDelete = new JButton("Delete");
+		JButton btnDelete = new JButton("  Delete");
+		btnDelete.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/delete.png")));
 		btnDelete.setFont(new Font("Gisha", Font.PLAIN, 22));
 		btnDelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnDelete.setBounds(42, 199, 217, 37);
+		btnDelete.setBounds(42, 199, 253, 37);
 		frmOop.getContentPane().add(btnDelete);
 		
 		JButton btnSaveAsKml = new JButton("Save as KML file");
+		btnSaveAsKml.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/kml-file-format-interface_24.png")));
 		btnSaveAsKml.setFont(new Font("Gisha", Font.PLAIN, 22));
-		btnSaveAsKml.setBounds(382, 138, 217, 37);
+		btnSaveAsKml.setBounds(365, 138, 253, 37);
 		frmOop.getContentPane().add(btnSaveAsKml);
 		
 		JButton btnSaveCombCsv = new JButton("Save as comb CSV");
+		btnSaveCombCsv.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/csv.png")));
 		btnSaveCombCsv.setFont(new Font("Gisha", Font.PLAIN, 22));
 		btnSaveCombCsv.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
-		});
-		btnSaveCombCsv.setBounds(382, 75, 217, 37);
-		frmOop.getContentPane().add(btnSaveCombCsv);
+		});   
 		
+		btnSaveCombCsv.setBounds(365, 77, 253, 37);
+		frmOop.getContentPane().add(btnSaveCombCsv);
+		 
 		JLabel lblNewJgoodiesTitle = DefaultComponentFactory.getInstance().createTitle("Input / Output:");
 		lblNewJgoodiesTitle.setFont(new Font("Gisha", Font.BOLD, 26));
 		lblNewJgoodiesTitle.setBounds(222, 24, 196, 29);
 		frmOop.getContentPane().add(lblNewJgoodiesTitle);
-		
-		JButton btnNewButton = new JButton("Filters");
+		 
+		JButton btnNewButton = new JButton("  Filters");
+		btnNewButton.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/filter.png")));
 		btnNewButton.setFont(new Font("Gisha", Font.PLAIN, 22));
-		btnNewButton.setBounds(382, 197, 217, 37);
+		btnNewButton.setBounds(365, 189, 253, 37);
 		frmOop.getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Algorithms");
+		JButton btnNewButton_1 = new JButton("  Algorithms");
+		btnNewButton_1.setIcon(new ImageIcon(ourGUI.class.getResource("/GUI_resource/algorithm.png")));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		btnNewButton_1.setFont(new Font("Gisha", Font.PLAIN, 22));
-		btnNewButton_1.setBounds(382, 258, 217, 37);
+		btnNewButton_1.setBounds(365, 258, 253, 37);
 		frmOop.getContentPane().add(btnNewButton_1);
 	}
 }
